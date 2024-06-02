dc611bea059641f7692c52a26b94f3e682b215a5
Sara Johnsson <sara86persson@hotmail.com>
2024-06-02 13:59:09 +0200

    fetched API from the dog api

---
+++ src/components/ApiService.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+
+
+const API_URL = 'https://api.thedogapi.com/v1/breeds';
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const fetchData = async () => {
+    try {
+        const response = await axios.get(API_URL, {
+            headers: {
+                'x-api-key': API_KEY,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error featching data: ', error);
+    }
+};
